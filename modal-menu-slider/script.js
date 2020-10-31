c5b9695712ce67986fdfeb4ec1feed1a0b09e832
web-devhact <73755639+web-devhact@users.noreply.github.com>
2020-10-31 23:34:08 +0530

    Update script.js

--- modal-menu-slider/script.js
+++ modal-menu-slider/script.js
@@ -15,3 +15,9 @@ function closeNavbar(e) {
     e.target !== navbar &&
     !navbar.contains(e.target)
   ) {
+ document.body.classList.toggle('show-nav');
+    document.body.removeEventListener('click', closeNavbar);
+  } else if (!document.body.classList.contains('show-nav')) {
+    document.body.removeEventListener('click', closeNavbar);
+  }
+}
