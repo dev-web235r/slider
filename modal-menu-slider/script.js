3f4dddf83a95ff063f5d76b3937566c42724dc59
web-devhact <73755639+web-devhact@users.noreply.github.com>
2020-10-31 23:32:44 +0530

    Update script.js

--- modal-menu-slider/script.js
+++ modal-menu-slider/script.js
@@ -4,3 +4,14 @@ const open = document.getElementById('open');
 const modal = document.getElementById('modal');
 const navbar = document.getElementById('navbar');
 
+// This function closes navbar if user clicks anywhere outside of navbar once it's opened
+// Does not leave unused event listeners on
+// It's messy, but it works
+function closeNavbar(e) {
+  if (
+    document.body.classList.contains('show-nav') &&
+    e.target !== toggle &&
+    !toggle.contains(e.target) &&
+    e.target !== navbar &&
+    !navbar.contains(e.target)
+  ) {
